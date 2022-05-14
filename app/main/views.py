2f88bc289bb20c4d864ba4e52943c6064bbc51fc
Fridah-kalee <fridahkalee@gmail.com>
2022-05-14 15:46:43 +0300

    create one to many relationship

--- app/main/views.py
+++ app/main/views.py
@@ -8,5 +8,5 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Fridah`s Blog Website'
+    return render_template('index.html',title = title)
--- app/models.py
+++ app/models.py
@@ -3,7 +3,16 @@ from .import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key = True)
+    title=db.Column(db.String(255))
+    blog = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Blog {self.blog}'
