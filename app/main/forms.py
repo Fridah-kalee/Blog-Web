2f776fee4d7e0e03f6720b1cde466b12b252edf6
Fridah-kalee <fridahkalee@gmail.com>
2022-05-15 08:32:54 +0300

    create pic upload path

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SelectField,SubmitField
+from wtforms.validators import InputRequired
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
     submit = SubmitField('Submit')
